db6abb1ec14977e8aaf48fe4265cf664f1dd243b
Vinayaksharma17 <vvsharma999@gmail.com>
2026-02-24 17:17:21 +0530

    refactor: update health check controller to use async-handler for improved error handling

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,5 +1,9 @@
 import { ApiResponse } from '../utils/api-response.js'
+import { asyncHandler } from '../utils/async-handler.js'
 
+/*
+This is the traditional way of writing with try catch block
+we can avoid this by writing async-handler in utils
 const healthCheck = (req, res) => {
   try {
     res.status(200).json(new ApiResponse(200, { message: 'Server is healthy and running' }))
@@ -7,5 +11,12 @@ const healthCheck = (req, res) => {
     console.error(error)
   }
 }
+ */
+
+//Below is the example of async-handler
+
+const healthCheck = asyncHandler((req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: 'server is running' }))
+})
 
 export { healthCheck }
