97893c024af506e304543f0e6b26deac57c68330
Vinayaksharma17 <vvsharma999@gmail.com>
2026-03-20 12:24:08 +0530

    fix: correct typo in emailVerificationToken field in user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,7 +51,7 @@ const userSchema = new Schema(
     forgetPasswordExpiry: {
       type: Date,
     },
-    emailVerificationToke: {
+    emailVerificationToken: {
       type: String,
     },
     emailVerificationExpiry: {
