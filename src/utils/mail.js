0477e98fd4c1d16bdba7bce0d83a643344e5fe00
Vinayaksharma17 <vvsharma999@gmail.com>
2026-03-20 12:24:02 +0530

    feat: implement email sending functionality using nodemailer

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,40 @@
 import Mailgen from 'mailgen'
+import nodemailer from 'nodemailer'
+
+const sendEmail = async (options) => {
+  const mailGen = new Mailgen({
+    theme: 'default',
+    product: {
+      name: 'Task Manager',
+      link: 'https://taskmanagerlink.com',
+    },
+  })
+  const emailTextual = mailGen.generatePlaintext(options.mailgenContent)
+  const emailHtml = mailGen.generate(options.mailgenContent)
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  })
+
+  const mail = {
+    from: 'mail.taskmanager@example.com',
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  }
+
+  try {
+    await transporter.sendMail(mail)
+  } catch (error) {
+    console.error("Email service failed siliently. Make sure that you have Correct Mailtrap credential correctly")
+  }
+}
 
 const emailVerificationMailgenContent = (username, verificationUrl) => {
   return {
