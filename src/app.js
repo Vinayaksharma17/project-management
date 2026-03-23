583d01e6ec57816c84886bee08f639a58e6db03f
Vinayaksharma17 <vvsharma999@gmail.com>
2026-03-23 12:13:18 +0530

    update routes path in app.js

--- src/app.js
+++ src/app.js
@@ -19,10 +19,24 @@ app.use(cors({
 // import the routes
 
 import healthCheckRouter from "./routes/healthcheck.route.js"
+import authRouter from "./routes/auth.routes.js"
 app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/auth", authRouter)
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+// global error handler
+app.use((err, req, res, _next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Something went wrong'
+  res.status(statusCode).json({
+    statusCode,
+    message,
+    errors: err.errors || [],
+    success: false,
+  })
 })
 
 export default app
