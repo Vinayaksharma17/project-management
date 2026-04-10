5891d281cb116a138f64ca72813a451c726d091e
Vinayaksharma17 <vvsharma999@gmail.com>
2026-04-10 07:35:12 +0530

    full curd operation for user schema

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,12 @@ import { User } from '../models/user.model.js'
 import { ApiResponse } from '../utils/api-response.js'
 import { ApiError } from '../utils/api-error.js'
 import { asyncHandler } from '../utils/async-handler.js'
-import { sendEmail, emailVerificationMailgenContent } from '../utils/mail.js'
+import {
+  sendEmail,
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+} from '../utils/mail.js'
+import jwt from 'jsonwebtoken'
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -137,6 +142,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 })
 
 const getCurrentUser = asyncHandler(async (req, res) => {
+  console.log('user: ', req.user)
+  console.log('req body: ', req.body)
   return res.status(200).json(new ApiResponse(200, req.user, 'Current user fetched successfully'))
 })
 const verifyEmail = asyncHandler(async (req, res) => {
@@ -169,7 +176,7 @@ const verifyEmail = asyncHandler(async (req, res) => {
 const resendEmailVerification = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user?._id)
   if (!user) throw new ApiError(404, 'User does not exist')
-  if (user.isEmailVerified) throw new ApiError('Email is already verified')
+  if (user.isEmailVerified) throw new ApiError(409, 'Email is already verified')
 
   const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken()
 
@@ -187,8 +194,128 @@ const resendEmailVerification = asyncHandler(async (req, res) => {
     ),
   })
 
-  return res.status(200).json(200, {}, 'Mail has been sent to your email ID')
+  return res.status(200).json(new ApiResponse(200, {}, 'Mail has been sent to your email ID'))
 })
-// const getCurrentUser = asyncHandler(async (req, res) => {})
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-export { registerUser, loginUser, logoutUser, getCurrentUser, verifyEmail }
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, 'Unauthorized Access')
+  }
+
+  try {
+    const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id)
+    if (!user) {
+      throw new ApiError(401, 'Invalid refresh token')
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, 'refresh token is expired')
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    }
+    const { accessToken, refreshToken: newRefreshToken } = await generateAccessAndRefreshTokens(
+      user._id,
+    )
+    user.refreshToken = newRefreshToken
+    await user.save()
+
+    return res
+      .status(200)
+      .cookie('accessToken', accessToken, options)
+      .cookie('refreshToken', newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          'Access token refreshed',
+        ),
+      )
+  } catch (error) {
+    throw new ApiError(401, 'Invalid refresh token')
+  }
+})
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+  const { email } = req.body
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new ApiError(401, 'Invalid refresh token')
+  }
+
+  const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken()
+  user.forgotPasswordToken = hashedToken
+  user.forgotPasswordExpiry = tokenExpiry
+
+  await user.save({ validateBeforeSave: false })
+
+  await sendEmail({
+    email: user?.email,
+    subject: 'Password reset request',
+    mailgenContent: forgotPasswordMailgenContent(
+      user.username,
+      `${process.env.FORGOT_PASSWORD_REDIRECT_URL}/${unHashedToken}`,
+    ),
+  })
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, 'Password reset mail has been sent on your mail'))
+})
+const resetForgotPassword = asyncHandler(async (req, res) => {
+  const { resetToken } = req.params
+  const { newPassword } = req.body
+
+  let hashedToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+  const user = await User.findOne({
+    forgotPasswordToken: hashedToken,
+    forgotPasswordExpiry: { $gt: Date.now() },
+  })
+
+  if (!user) {
+    throw new ApiError(499, 'Token is invalid or expired')
+  }
+
+  user.forgotPasswordExpiry = undefined
+  user.forgotPasswordToken = undefined
+
+  user.password = newPassword
+  await user.save({ validateBeforeSave: false })
+
+  return res.status(200).json(new ApiResponse(200, {}, 'Password reset successfully'))
+})
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body
+
+  const user = await User.findById(req.user?._id)
+  const isPasswordValid = await user.isPasswordCorrect(oldPassword)
+
+  if (!isPasswordValid) {
+    throw new ApiError(400, 'Invalid old password')
+  }
+
+  user.password = newPassword
+  await user.save({ validateBeforeSave: false })
+
+  return res.status(200).json(new ApiResponse(200, {}, 'Password changed successfully'))
+})
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getCurrentUser,
+  verifyEmail,
+  resendEmailVerification,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  resetForgotPassword,
+  changeCurrentPassword,
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,24 @@
 import { Router } from 'express'
-import { loginUser, registerUser, logoutUser } from '../controllers/auth.controller.js'
+import { loginUser, registerUser, logoutUser, getCurrentUser, verifyEmail, resendEmailVerification, refreshAccessToken, forgotPasswordRequest, resetForgotPassword, changeCurrentPassword } from '../controllers/auth.controller.js'
 import { validate } from '../middlewares/validator.middleware.js'
-import { userRegisterValidator, userLoginValidator } from '../validators/index.js'
+import { userRegisterValidator, userLoginValidator, userForgotPasswordValidator, userResetForgotPasswordValidator, userChangeCurrentPasswordValidator } from '../validators/index.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
+//unsecured routes
 router.route('/register').post(userRegisterValidator(), validate, registerUser)
 router.route('/login').post(userLoginValidator(), validate, loginUser)
+router.route('/verify-email/:verificationToken').get(verifyEmail)
+// router.route('/email-verification').post(resendEmailVerification)
+router.route('/refresh-token').post(refreshAccessToken)
+router.route('/forgot-password').post(userForgotPasswordValidator(), validate, forgotPasswordRequest)
+router.route('/reset-password/:resetToken').post(userResetForgotPasswordValidator(), validate, resetForgotPassword)
+
+//secured routes
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/current-user').post(verifyJWT, getCurrentUser)
+router.route('/change-password').post(verifyJWT, userChangeCurrentPasswordValidator(), validate, changeCurrentPassword)
+router.route('/resend-email')
 
 export default router
